c9dbb719bb2206c447098711eaf5e1b7e60d59bd
harikrishnavizhaa <harikrishnacsbs@gmail.com>
2026-05-07 22:20:50 +0530

    remove Vizhaa_organizer project directory

--- screens/TabPlaceholders.tsx
+++
@@ -1,4 +0,0 @@
-// Re-export the actual screen components
-export { default as ProfileScreen } from './ProfileScreen';
-export { default as HistoryScreen } from './HistoryScreen';
-export { default as PaymentScreen } from './PaymentScreen';
